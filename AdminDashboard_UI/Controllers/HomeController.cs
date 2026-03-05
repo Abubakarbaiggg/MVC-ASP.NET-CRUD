46849ade8eb1db7ec493542915c12d08a041dc1f
Abubakar-telecard <Abubakar2211f@aptechorangi.com>
2026-03-05 11:30:55 +0500

    Complete Authentication In Project

--- AdminDashboard_UI/Controllers/HomeController.cs
+++ AdminDashboard_UI/Controllers/HomeController.cs
@@ -4,22 +4,73 @@ using System.Diagnostics;
 
 namespace AdminDashboard_UI.Controllers
 {
+
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Registration()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Dashboard()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+        public IActionResult PasswordIncorrect()
+        {
+            return View();
+        }
+        public IActionResult UserNotFound()
+        {
+            return View();
+        }
+        public IActionResult ExistingUserFound()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterUser(string username,string email,string password,string confirmpassword)
+        {
+            if(password != confirmpassword)
+            {
+                return View("PasswordIncorrect");
+            }
+            var existingUser = _context.Users.FirstOrDefault(u => u.Name == username || u.Email == email);
+            if(existingUser != null)
+            {
+                return View("ExistingUserFound");
+            }
+            _context.Users.Add(new Users
+            {
+                Name = username,
+                Email = email,
+                Password = password,
+                Role = "User"
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Name == username);
+            if(user == null)
+            {
+                return View("UserNotFound");
+            }
+            if(user.Password != password)
+            {
+                return View("PasswordIncorrect");
+            }
+            return View("Dashboard");
         }
     }
 }
